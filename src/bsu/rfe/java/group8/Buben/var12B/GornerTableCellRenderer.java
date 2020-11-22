e5ff7ba32b5274dcd6f00bdb17918178f03ae9b9
Ivan <ivan.buben1994@gmail.com>
2020-11-22 18:14:41 +0300

    Last commit: third col, change color value

--- src/bsu/rfe/java/group8/Buben/var12B/GornerTableCellRenderer.java
+++ src/bsu/rfe/java/group8/Buben/var12B/GornerTableCellRenderer.java
@@ -18,7 +18,8 @@ public class GornerTableCellRenderer implements TableCellRenderer {
     // (иголкой). Применяется аналогия поиска иголки в стоге сена, в роли
     // стога сена - таблица
     private String needle = null;
-    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+
     public GornerTableCellRenderer() {
         // Показывать только 5 знаков после запятой
         formatter.setMaximumFractionDigits(5);
@@ -37,24 +38,51 @@ public class GornerTableCellRenderer implements TableCellRenderer {
         // Установить выравнивание надписи по левому краю панели
         panel.setLayout(new FlowLayout(FlowLayout.LEFT));
     }
+
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
         // Преобразовать double в строку с помощью форматировщика
         String formattedDouble = formatter.format(value);
         // Установить текст надписи равным строковому представлению числа
         label.setText(formattedDouble);
-        if (col==1 && needle!=null && needle.equals(formattedDouble)) {
-            // Номер столбца = 1 (т.е. второй столбец) + иголка не null
-            // (значит что-то ищем) +
-            // значение иголки совпадает со значением ячейки таблицы -
-            // окрасить задний фон панели в красный цвет
-            panel.setBackground(Color.RED);
-        } else {
-            // Иначе - в обычный белый
-            panel.setBackground(Color.WHITE);
+        int index = formattedDouble.length() - formattedDouble.indexOf(".") - 1;
+        int indexDot = formattedDouble.indexOf(".");
+        int count = 0;
+
+
+        if (indexDot != -1) {
+            for (int i = indexDot + 1; i < formattedDouble.length(); i++) {
+                char letter = formattedDouble.charAt(i);
+                if (letter == '1') {
+                    count += 1;
+                }
+                if (letter == '3') {
+                    count += 1;
+                }
+                if (letter == '5') {
+                    count += 1;
+                }
+            }
         }
+
+        if (count == formattedDouble.length() - formattedDouble.indexOf(".") - 1)
+        {
+            if (col==1 && needle!=null && needle.equals(formattedDouble))
+                panel.setBackground(Color.RED);
+            else panel.setBackground(Color.PINK);
+        }
+        else
+        {
+            if (col==1 && needle!=null && needle.equals(formattedDouble))
+                panel.setBackground(Color.RED);
+            else panel.setBackground(Color.WHITE);
+        }
+
         return panel;
     }
+
     public void setNeedle(String needle) {
         this.needle = needle;
     }
 }
+
+
--- src/bsu/rfe/java/group8/Buben/var12B/GornerTableModel.java
+++ src/bsu/rfe/java/group8/Buben/var12B/GornerTableModel.java
@@ -10,36 +10,34 @@ public class GornerTableModel extends AbstractTableModel {
     private Double step;
     private double result[] = new double[1];
 
-    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients)
+    {
         this.from = from;
         this.to = to;
         this.step = step;
         this.coefficients = coefficients;
     }
-
-    public Double getFrom() {
+    public Double getFrom()
+    {
         return from;
     }
-
-    public Double getTo() {
+    public Double getTo()
+    {
         return to;
     }
-
-    public Double getStep() {
+    public Double getStep()
+    {
         return step;
     }
-
-    public int getColumnCount() {
-        // В данной модели два столбца
-        return 2;
+    public int getColumnCount()
+    {
+        return 3;
     }
-
-    public int getRowCount() {
-        // Вычислить количество точек между началом и концом отрезка
-        // исходя из шага табулирования
-        return new Double(Math.ceil((to - from) / step)).intValue() + 1;
+    public int getRowCount()
+    {
+        // Вычислить количество точек между началом и концом отрезка исходя из шага табулирования
+        return new Double(Math.ceil((to-from)/step)).intValue()+1;
     }
-
     public Object getValueAt(int row, int col)
     {
         double x = from + step * row;
@@ -79,18 +77,27 @@ public class GornerTableModel extends AbstractTableModel {
         }
     }
 
-    public String getColumnName(int col) {
-        switch (col) {
-            case 0:// Название 1-го столбца
+    public String getColumnName(int col)
+    {
+        switch (col)
+        {
+            case 0:
                 return "Значение X";
-            default:
-                // Название 2-го столбца
+            case 1:
                 return "Значение многочлена";
+            default:
+                return "Значение простое?";
         }
     }
-
-    public Class<?> getColumnClass(int col) {
-        // И в 1-ом и во 2-ом столбце находятся значения типа Double
-        return Double.class;
+    // необходимо предоставить сведения о типе данных в столбцах, в противном случае они будут отображены как строки
+    public Class<?> getColumnClass(int col)
+    {
+        switch (col)
+        {
+            case 2:
+                return Boolean.class;
+            default:
+                return Double.class;
+        }
     }
 }
