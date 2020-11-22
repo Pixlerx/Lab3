84f190deaa663a3d2f9bbad7920b9f6a9d7299f4
Ivan <ivan.buben1994@gmail.com>
2020-11-22 16:10:49 +0300

    creat about programm

--- src/bsu/rfe/java/group8/Buben/var12B/GornerTableModel.java
+++ src/bsu/rfe/java/group8/Buben/var12B/GornerTableModel.java
@@ -8,9 +8,9 @@ public class GornerTableModel extends AbstractTableModel {
     private Double from;
     private Double to;
     private Double step;
+    private double result[] = new double[1];
 
-    public GornerTableModel(Double from, Double to, Double step,
-                            Double[] coefficients) {
+    public GornerTableModel(Double from, Double to, Double step, Double[] coefficients) {
         this.from = from;
         this.to = to;
         this.step = step;
@@ -40,20 +40,42 @@ public class GornerTableModel extends AbstractTableModel {
         return new Double(Math.ceil((to - from) / step)).intValue() + 1;
     }
 
-    public Object getValueAt(int row, int col) {
-        // Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+    public Object getValueAt(int row, int col)
+    {
         double x = from + step * row;
-        if (col == 0) {
-        // Если запрашивается значение 1-го столбца, то это X
-            return x;
-        } else {
-            // Если запрашивается значение 2-го столбца, то это значение
-            // многочлена
-            Double result = 0.0;
-            // Вычисление значения в точке по схеме Горнера.
-            // Вспомнить 1-ый курс и реализовать
-            // ...
-            return result;
+        switch (col)
+        {
+            case 0:
+                return x;
+            case 1:
+            {
+                result[0] = 0.0;
+                for (int i = 0; i < coefficients.length; i++)
+                {
+                    result[0] += Math.pow(x, coefficients.length - 1 - i) * coefficients[i];
+                }
+                return result[0];
+            }
+            default:
+            {
+                result[0] = 0.0;
+                for (int i = 0; i < coefficients.length; i++)
+                    result[0] += Math.pow(x, coefficients.length - 1 - i) * coefficients[i];
+
+                int temp = 0;
+                boolean flag = true;
+                for (int j = 2; j <= result[0] - 1; j++)
+                {
+                    temp = (int)result[0] % j;
+                    if (temp == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag==true) return true;
+                else return false;
+            }
         }
     }
 
